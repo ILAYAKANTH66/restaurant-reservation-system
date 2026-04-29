fa0845105fe396b322d22105aadec19906212ced
ILAYAKANTH66 <ilayakanth666@gmail.com>
2026-04-29 23:06:32 +0530

    style changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
           {/* Desktop Navigation */}
           <div className="hidden md:flex items-center space-x-8">
             <Link to="/restaurants" className="text-gray-700 hover:text-primary-600 transition-colors">
-              Restaurants
+              RESTAURENTS
             </Link>
             
             {isAuthenticated ? (
